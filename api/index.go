2aa12c2c269b5a2e1eef7ba4eb154abca2860e81
Andre Soares <andregsilv@gmail.com>
2023-10-05 23:21:55 -0300

    time now

--- api/index.go
+++ api/index.go
@@ -3,8 +3,9 @@ package handler
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+	fmt.Fprintf(w, "<h1>Bom dia. Agora são %s horas</h1>", time.Now())
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mniak/testejair
+
+go 1.21.1
